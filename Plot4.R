c1f6562c2099e9c8402d70bb7d645bee9c76d953
emiliosjlc <emiliosjlc@hotmail.com>
2015-05-08 13:34:44 +0200

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,17 @@
+install.packages("sqldf")
+library("sqldf", lib.loc="~/R/win-library/3.1")
+hpc <- read.csv.sql(file = "household_power_consumption.txt", sql = "select * from file where Date = '1/2/2007' or Date='2/2/2007'", header =TRUE,sep=";")
+dates<-hpc$Date
+times<-hpc$Time
+hpc$Date<-paste(dates,times)
+hpc$Date<-strptime(hpc$Date, "%d/%m/%Y %H:%M:%S")
+par(mfrow=c(2,2))
+plot(hpc$Date,hpc$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(hpc$Date,hpc$Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(hpc$Date,hpc$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", col="Black")
+lines(hpc$Date,hpc$Sub_metering_3, type="l", col="Blue")
+lines(hpc$Date,hpc$Sub_metering_2, type="l", col="Red")
+legend("topright", lty=1,col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_3", "Sub_metering_3"))
+plot(hpc$Date,hpc$Global_reactive_power, type="l", xlab="datetime", ylab="Global reactive power", col="Black")
+dev.copy(png, width=480, height=480, file="Plot4.png")
+dev.off()
